3fdafccea881359d40c3d5576d09b1fab93ab229
Joshua DiChello <jddichello@gmail.com>
2024-09-25 18:19:17 -0500

    changed hello world message

--- test-project/test-project/obj/Debug/net8.0/test-project.AssemblyInfo.cs
+++ test-project/test-project/obj/Debug/net8.0/test-project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test-project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+374bb7d1b936866087efad22572d03dd017016d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a6de57d75827c61e405c48a1636a933c5cf5194")]
 [assembly: System.Reflection.AssemblyProductAttribute("test-project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test-project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
